8f8bec7b98797d92865d110931aa182f405197f5
DaryBob <netcrafter1@gmail.com>
2014-12-02 17:18:48 +0100

    Some basic stuffs :D

---
+++ com/netcrafter/mod/items/RPGItems.java
@@ -0,0 +1,20 @@
+package com.netcrafter.mod.items;
+
+import com.netcrafter.mod.LidlMelonRPG;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
+
+public class RPGItems extends Item {
+	
+	public RPGItems() {
+		this.setCreativeTab(LidlMelonRPG.rpgMaterialTab);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister iconRegister) {
+		this.itemIcon = iconRegister.registerIcon("netcrafter:" + this.getUnlocalizedName().substring(5));
+	}
+}
